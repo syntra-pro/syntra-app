ff33b82fa083447dc0a9a9a1d1403d9b62975dee
LeoSagan <raiseerco@gmail.com>
2024-08-30 05:15:00 -0400

    feat(UI) Fixes dao cards

--- src/app/components/DaoCard.tsx
+++ src/app/components/DaoCard.tsx
@@ -4,7 +4,7 @@ import React from "react";
 interface DaoCardProps {
   colour: string;
   path: string;
-  collabs: number;
+  drafts: number;
   projects: number;
   name: string;
   disabled: boolean;
@@ -13,36 +13,34 @@ interface DaoCardProps {
 const DaoCard: React.FC<DaoCardProps> = ({
   colour,
   path,
-  collabs,
+  drafts,
   projects,
   name,
   disabled,
 }) => {
   return disabled ? (
     <div
-      className="flex opacity-70 items-center gap-2 rounded-lg
-   text-sm font-medium text-gray-500 transition-colors dark:text-gray-400
+      className="flex opacity-50 items-center gap-2 rounded-lg
+   text-sm font-medium text-gray-500   dark:text-gray-400 shadow-md
    bg-stone-100
    "
     >
-      <div className="rounded-lg p-4 w-64 h-60">
+      <div className="rounded-lg p-4 w-64 h-56">
         <div className={`w-56 h-32 mb-2 rounded-lg ${colour}`}></div>
-        <p className="text-2xl font-semibold">{name}</p>
-        <p className="text-xs">{collabs} Collaborators</p>
-        <p className="text-xs">{projects} Projects</p>
+        <div className="text-2xl font-semibold">{name}</div>
+        <div className="text-xs mt-1">{drafts} Drafts</div>
       </div>
     </div>
   ) : (
     <Link
-      className="flex items-center gap-2 bg- border border-stone-500 rounded-lg
+      className="flex items-center gap-2 border border-stone-200 rounded-lg shadow-md
    text-sm font-medium text-gray-500 transition-colors dark:text-gray-400"
       href={path}
     >
-      <div className="rounded-lg p-4 w-64 h-60">
+      <div className="rounded-lg p-4 w-64 h-56">
         <div className={`w-56 h-32 mb-2 rounded-lg ${colour}`}></div>
-        <p className="text-2xl font-semibold">{name}</p>
-        <p className="text-xs">{collabs} Collaborators</p>
-        <p className="text-xs">{projects} Projects</p>
+        <div className="text-2xl font-semibold">{name}</div>
+        <div className="text-xs mt-1">{drafts} Drafts</div>
       </div>
     </Link>
   );
